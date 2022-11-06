0e780171d73cb18b9a1f13c1ac69a772ec1790dc
NicolasYanB <nicolas07.yan@gmail.com>
2022-11-06 13:22:13 -0300

    Add TextInput to addTaskModal

--- src/components/addTaskModal.jsx
+++ src/components/addTaskModal.jsx
@@ -1,15 +1,25 @@
-import {View, Modal, Text} from 'react-native';
+import {View, Modal, TextInput, StyleSheet} from 'react-native';
 
 function AddTaskModal(){
     return(
         <View>
-            <Modal>
-                <Text>Modal test</Text>
+            <Modal transparent={true}>
+                <View style={styles.modalView}>
+                    <TextInput style={styles.input} placeholder={'Add a task'}/>
+                </View>
             </Modal>
         </View>
     );
 }
 
-//const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    modalView: {
+        backgroundColor: 'lightblue'
+    },
+    input: {
+        fontSize: 17,
+        margin: '3%'
+    }
+});
 
 export default AddTaskModal;
