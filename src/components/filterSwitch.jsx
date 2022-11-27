eab253239baf6af7349084e7c3b9fbab8b37dce4
NicolasYanB <nicolas07.yan@gmail.com>
2022-11-27 15:22:21 -0300

    Switch styled to the right

--- src/components/filterSwitch.jsx
+++ src/components/filterSwitch.jsx
@@ -1,11 +1,17 @@
-import { View, Switch } from "react-native";
+import { View, Switch,  StyleSheet } from "react-native";
 
 function FilterSwitch(){
     return (
-        <View>
+        <View style={styles.switch}>
             <Switch/>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    switch: {
+        alignItems: 'flex-start'
+    }
+});
 
 export default FilterSwitch
